35681f9b649e7e0ab475de700dd736d831e6e761
themahabur <mahabursoikot04@gmail.com>
2026-05-16 19:33:34 +0600

    Remove authorization from equipment retrieval route

--- src/modules/equipment/equipment.route.ts
+++ src/modules/equipment/equipment.route.ts
@@ -5,7 +5,7 @@ import authorize from "../../middleware/authorize";
 const equipmentRouter: Router = Router();
 
 equipmentRouter.post("/",authorize("ADMIN"), equipmentController.createEquipment);
-equipmentRouter.get("/", authorize("ADMIN" , "USER"), equipmentController.getEquipment);
+equipmentRouter.get("/", equipmentController.getEquipment);
 equipmentRouter.get("/:id", authorize("ADMIN", "USER"), equipmentController.getEquipmentById);
 
 export default equipmentRouter;
